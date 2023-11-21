7d0a0950ed34efc62ec24797b2c6375567557edb
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2023-11-21 19:43:51 -0300

    fix: clean Register Professional Screen

--- lib/ui/registerscreenpro.dart
+++ lib/ui/registerscreenpro.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 //import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_application_saude_app/utils.dart';
 
 class RegisterScreenPro extends StatelessWidget {
