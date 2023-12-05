b0269f52e1be27ff250fc287c4ba6dd24976f040
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2023-12-05 00:49:51 -0300

    fix: fix ugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/splash.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import "package:flutter/material.dart";
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:agenda_crud/app/injection.dart';
 
-void main() async {
-  await Firebase.initializeApp();
+void main() {
+//void main() async {
+//  setupInjection();
+//  await Firebase.initializeApp();
 
-  FirebaseFirestore.instance.collection('contact').doc().set({
-    'name': 'Elildes',
-    'phone': '(84) 9 9654-8765',
-    'e-mail': 'elildes@elildes.com'
-  });
+//  FirebaseFirestore.instance.collection('contact').doc().set({
+//    'name': 'Elildes',
+//    'phone': '(84) 9 9654-8765',
+//    'e-mail': 'elildes@elildes.com'
+//  });
 
   runApp(const SaudeApp());
 }
