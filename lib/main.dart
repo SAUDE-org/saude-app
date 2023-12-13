09a72830c82e5b988d2ad15c0130bb0404b18764
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2023-12-12 22:19:54 -0300

    feat: dependence, url and key api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/splash.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const SaudeApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://uglsqlbqkszsgehbmbeg.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnbHNxbGJxa3N6c2dlaGJtYmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0MjcxMjUsImV4cCI6MjAxODAwMzEyNX0.NMZB7yVfSmQte6z6sIhvW6kwA4CUqwrn-7vshjaMptM',
+  );
+  runApp(SaudeApp());
+}
+
+//void main() => runApp(const SaudeApp());
 
 class SaudeApp extends StatelessWidget {
   const SaudeApp({super.key});
